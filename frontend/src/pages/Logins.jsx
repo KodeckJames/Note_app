64ad17d731d050eca3e44e02c87aa81fd7a13880
Kodeck James <kodeckjames095@gmail.com>
2024-05-12 23:17:50 +0300

    first commit

---
+++ frontend/src/pages/Logins.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Form from '../components/Form'
+
+function Logins() {
+  return (
+    <Form route="api/token/" method="login" />
+  )
+}
+
+export default Logins
---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Form from '../components/Form'
+
+
+function Register () {
+  return (
+   <Form route="api/user/register/" method="register" />
+  )
+}
+
+export default Register
